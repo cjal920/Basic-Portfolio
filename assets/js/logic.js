7348215ffa6e84652c031712afaf5904e9c9be0d
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-05-09 23:14:47 -0400

    updating basic portfolio

---
+++ assets/js/logic.js
@@ -0,0 +1,16 @@
+document.getElementById("about").onlick = function () {
+    location.href = "index.html";
+};
+
+document.getElementById("portfolio").onclick = function () {
+    location.href = "portfolio.html";
+};
+
+document.getElementById("contact").onlick = function () {
+    location.ref = "contact.html";
+};
+
+document.getElementById("submit").onclick = function () {
+    location.href = "index.html"
+}
+
